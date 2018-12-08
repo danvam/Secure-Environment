6c5002e2771631fcb72e4b7ed2ed32a465d1caa5
Daniel Valle <dvm1983@gmail.com>
2018-12-08 13:00:37 +0100

    initial commit

---
+++ src/main/java/es/mjsp/tfm/SecureEnvironmentApplication.java
@@ -0,0 +1,12 @@
+package es.mjsp.tfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureEnvironmentApplication.class, args);
+	}
+}
